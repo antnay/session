9c78ff9bbc9f1c17a9ea9af71d14865f544c8528
anthony <antnay@uw.edu>
2024-03-20 20:21:26 -0700

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use fzf_wrapped::{Fzf, run_with_output};
+use fzf_wrapped::{run_with_output, Fzf};
 use std::fs;
 use std::fs::File;
 use std::io;
@@ -17,10 +17,9 @@ fn get_sub_dirs(out_dirs: &mut Vec<String>, dir: &str, layers: i8) -> io::Result
         let entry = entry?;
         let path = entry.path();
         if entry.file_type()?.is_dir() {
-            let path_str = path.display().to_string();
-            // println!("{}", path_str);
-            out_dirs.push(path_str.clone());
-            let sub_results = get_sub_dirs(out_dirs, &path_str, layers - 1)?;
+            let basename = path.display().to_string();
+            out_dirs.push(basename.clone());
+            let sub_results = get_sub_dirs(out_dirs, &basename, layers - 1)?;
             results.extend(sub_results);
         }
     }
@@ -41,52 +40,43 @@ fn parse(out_dirs: &mut Vec<String>, dir_yaml: String) -> Result<Vec<String>, Er
         let name = entry["name"].as_str().unwrap();
         let layers = entry["layers"].as_i64().unwrap() as i8;
         let cur_dir = &format!("{}{}", HOME, name);
-        // println!("{}", cur_dir);
         get_sub_dirs(out_dirs, cur_dir, layers).expect("Something went awry");
     }
     Ok(out_dirs.to_vec())
 }
 
-// fn new_session(selected_path: &str) {
-//
-// }
-
 fn main() {
     let mut out_dirs: Vec<String> = Vec::new();
     let _ = parse(&mut out_dirs, "./etc/dirs.yaml".to_string());
 
-    // for path in out_dirs.clone() {
-    //     println!("{}", path);
-    // }
-
-    let users_selection: String = run_with_output(Fzf::default(), out_dirs).expect("Something went wrong!");
-    let path_vec: Vec<&str> = &users_selection.split('/');
-    let basename = path_vec.last();
-
+    let users_selection: String =
+        run_with_output(Fzf::default(), out_dirs).expect("Something went wrong!");
     if users_selection.is_empty() {
         std::process::exit(0)
     }
-    let status = Tmux::with_command(HasSession::new().target_session(&users_selection))
+
+    let (remaing, basename) = users_selection.rsplit_once('/').unwrap();
+    let (_, parent) = remaing.rsplit_once('/').unwrap();
+    let session_name = format!("{}/{}", parent, basename);
+
+    let status = Tmux::with_command(HasSession::new().target_session(&session_name))
         .status()
         .unwrap()
         .success();
 
-    if status {
-        Tmux::with_command(SwitchClient::new().target_session(&users_selection))
-            .status()
-            .unwrap();
-    } else {
+    if !status {
         Tmux::new()
             .add_command(
                 NewSession::new()
-                    .session_name(&users_selection)
+                    .detached()
+                    .session_name(&session_name)
                     .start_directory(&users_selection),
             )
             .output()
             .unwrap();
     }
-
-    // Tmux::with_command(SwitchClient::new().target_session(basename));
-
-    println!("\nName: {}", users_selection);
+    Tmux::with_command(SwitchClient::new().target_session(&session_name))
+        .status()
+        .unwrap();
+        std::process::exit(0)
 }
