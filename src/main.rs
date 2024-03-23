6f71e43475549f33f57663dcd3270d2b795bdef8
Anthony <antnay@uw.edu>
2024-03-23 00:46:10 -0700

    renamed current-release branch to new-features

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,32 @@
-use fzf_wrapped::{run_with_output, Fzf};
+use fzf_wrapped::{run_with_output, Border, Color, Fzf};
+use std::collections::HashSet;
 use std::fs;
 use std::fs::File;
 use std::io;
 use std::io::{Error, Read};
+use std::ops::Deref;
 use std::path::PathBuf;
+use std::process::exit;
 use std::sync::{Arc, Mutex};
 use std::thread::{self, JoinHandle};
-use tmux_interface::{HasSession, NewSession, SwitchClient, Tmux};
+use tmux_interface::{start_server, AttachSession, HasSession, NewSession, Tmux};
 use yaml_rust::YamlLoader;
 
-// TODO: Handle non existent files
-// TODO: Handle empty files
-// TODO: Handle no entries in file
+// TODO: Add support for other directories outside of HOME
 
 const DIRS: &str = "session-directories.yaml";
+
+fn main() {
+    let home = get_home_dir() + "/";
+    // let orig_out_dirs = Arc::new(Mutex::new(vec![home.to_owned()]));
+    let mut s_directories: HashSet<String> = HashSet::with_capacity(1);
+    s_directories.insert(home.clone());
+    let orig_out_dirs: Arc<Mutex<HashSet<String>>> = Arc::new(Mutex::new(s_directories));
+    let path_to_dir_list = home.to_owned() + DIRS;
+    let _ = parse(Arc::clone(&orig_out_dirs), home, path_to_dir_list);
+    // let out_dirs = orig_out_dirs.lock().unwrap().clone();
+    tmux_session(fzf_search(orig_out_dirs.lock().unwrap().deref().clone()));
+}
 
 fn get_home_dir() -> String {
     simple_home_dir::home_dir()
@@ -22,72 +35,74 @@ fn get_home_dir() -> String {
         .to_string()
 }
 
-fn get_sub_dirs_mul_layer(
-    out_dirs: &mut Vec<String>,
-    dir: PathBuf,
-    layers: i8,
-) -> io::Result<Vec<String>> {
-    let mut results = Vec::new();
-    if layers == 0 {
-        return Ok(out_dirs.to_vec());
-    }
-    for entry in fs::read_dir(dir)? {
-        let entry = entry?;
-        let path = entry.path();
-        if entry.file_type()?.is_dir() {
-            let file_name = entry.file_name();
-            if let Some(name_str) = file_name.to_str() {
-                if !name_str.starts_with('.') {
-                    let basename = &path.display().to_string();
-                    // println!("{}", basename);
-                    out_dirs.push(basename.clone());
-                    let sub_results = get_sub_dirs_mul_layer(out_dirs, path, layers - 1)?;
-                    results.extend(sub_results);
-                }
-            }
-        }
-    }
-    Ok(results)
-}
-
-fn add_to_dirs(out_dirs: &mut Vec<String>, dir: PathBuf) -> io::Result<Vec<String>> {
-    // println!("{:?}", dir);
-    out_dirs.push(dir.to_str().unwrap().to_string());
-    Ok(out_dirs.to_vec())
-}
-
-fn parse(out_dirs: Arc<Mutex<Vec<String>>>, home: String, dir_yaml: String) -> Result<(), Error> {
-    // let mut file = File::open(dir_yaml)?;
+fn parse(
+    out_dirs: Arc<Mutex<HashSet<String>>>,
+    home: String,
+    dir_yaml: String,
+) -> Result<(), Error> {
     let mut file = match File::open(dir_yaml) {
-        Err(why) => panic!("\ncouldn't open {}: {}\n", DIRS, why),
+        Err(why) => {
+            eprintln!("\ncouldn't open {}: {}\n", DIRS, why);
+            exit(1);
+        }
         Ok(file) => file,
     };
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
-    // let docs = YamlLoader::load_from_str(&contents).unwrap();
     let docs = match YamlLoader::load_from_str(&contents) {
-        Err(why) => panic!("\ncouldn't parse {}: {}\n", DIRS, why),
+        Err(why) => {
+            eprintln!("\ncouldn't parse {}: {}\n", DIRS, why);
+            exit(1);
+        }
         Ok(docs) => docs,
     };
     let doc: &yaml_rust::Yaml;
     if docs.is_empty() {
-        panic!("\nuh oh {} is completely empty!\nconsider adding some directories in the format:\n\ndirectories:\n  - name: <directory excluding home path>\n    layers: <number of layers>\n", DIRS)
+        eprintln!("uh oh {} is completely empty!\nconsider adding some directories in the format:\n\ndirectories:\n  - name: <directory excluding home path>\n    layers: <number of layers>", DIRS);
+        exit(1);
     } else {
         doc = &docs[0];
     }
-    let directories = &doc["directories"].as_vec().unwrap_or_else(|| panic!("\nyikes, there doesn't seem to be any entries in {}!\nmake sure you have are following the format:\n\ndirectories:\n  - name: <directory excluding home path>\n    layers: <number of layers>\n", DIRS));
-
-    
-    let mut handles: Vec<JoinHandle<Result<Vec<String>, Error>>> = vec![];
-
+    let directories;
+    match doc["directories"].as_vec() {
+        Some(dir) => directories = dir,
+        None => {
+            eprintln!("yikes, there doesn't seem to be any entries in 'directories'!\nmake sure you are following the format:\n\ndirectories:\n  - name: <directory excluding home path>\n    layers: <number of layers>");
+            exit(1);
+        }
+    }
+    let mut handles: Vec<JoinHandle<Result<HashSet<String>, Error>>> = vec![];
     for entry in directories.iter() {
-        let name = entry["name"].as_str().unwrap().to_string();
-        let layers = entry["layers"].as_i64().unwrap() as i8;
+        let name_out = entry["name"].as_str();
+        let name: String;
+        match name_out {
+            Some(name_from_entry) => name = name_from_entry.to_string(),
+            None => {
+                eprintln!(
+                    "oh shoot, an entry is missing a value for 'name' in {}! Skipping.",
+                    DIRS
+                );
+                continue;
+            }
+        }
+        let layers_out = entry["layers"].as_i64();
+        let layers: i8;
+        match layers_out {
+            Some(layers_from_entry) => layers = layers_from_entry as i8,
+            None => {
+                eprintln!(
+                    "aw man, an entry is missing a value for 'layers' in {}! Skipping.",
+                    DIRS
+                );
+                continue;
+            }
+        }
+
         let home_clone = home.clone();
         let out_dirs_clone = Arc::clone(&out_dirs);
         let handle = thread::spawn(move || {
             let cur_dir_path = PathBuf::from(&format!("{}{}", home_clone, name));
-            let result: Result<Vec<String>, Error>;
+            let result: Result<HashSet<String>, Error>;
             if layers == 0 {
                 result = add_to_dirs(&mut out_dirs_clone.lock().unwrap(), cur_dir_path);
             } else {
@@ -108,10 +123,45 @@ fn parse(out_dirs: Arc<Mutex<Vec<String>>>, home: String, dir_yaml: String) -> R
     Ok(())
 }
 
+fn get_sub_dirs_mul_layer(
+    out_dirs: &mut HashSet<String>,
+    dir: PathBuf,
+    layers: i8,
+) -> io::Result<HashSet<String>> {
+    let mut results = HashSet::new();
+    if layers == 0 {
+        return Ok(out_dirs.deref().clone());
+    }
+    for entry in fs::read_dir(dir)? {
+        let entry = entry?;
+        let path = entry.path();
+        if entry.file_type()?.is_dir() {
+            let file_name = entry.file_name();
+            if let Some(name_str) = file_name.to_str() {
+                if !name_str.starts_with('.') {
+                    let basename = &path.display().to_string();
+                    // println!("{}", basename);
+                    out_dirs.insert(basename.clone());
+                    let sub_results = get_sub_dirs_mul_layer(out_dirs, path, layers - 1)?;
+                    results.extend(sub_results);
+                }
+            }
+        }
+    }
+    Ok(results)
+}
+
+fn add_to_dirs(out_dirs: &mut HashSet<String>, dir: PathBuf) -> io::Result<HashSet<String>> {
+    // println!("{:?}", dir);
+    out_dirs.insert(dir.to_str().unwrap().to_string());
+    Ok(out_dirs.deref().clone())
+}
+
 fn tmux_session(users_selection: String) {
     let (remaining, basename) = users_selection.rsplit_once('/').unwrap();
     let (_, parent) = remaining.rsplit_once('/').unwrap();
     let session_name = format!("{}/{}", parent, basename);
+    start_server!();
     let status = Tmux::with_command(HasSession::new().target_session(&session_name))
         .status()
         .unwrap()
@@ -127,24 +177,25 @@ fn tmux_session(users_selection: String) {
             .output()
             .unwrap();
     }
-    Tmux::with_command(SwitchClient::new().target_session(&session_name))
+    Tmux::with_command(AttachSession::new().target_session(&session_name))
         .status()
         .unwrap();
     std::process::exit(0)
 }
 
-fn main() {
-    let home = get_home_dir() + "/";
-    let orig_out_dirs = Arc::new(Mutex::new(Vec::new()));
-    let path_to_dir_list = home.to_owned() + DIRS;
-    // println!("path to dir list {}", path_to_dir_list);
-    let _ = parse(Arc::clone(&orig_out_dirs), home, path_to_dir_list);
-    let out_dirs = orig_out_dirs.lock().unwrap().clone();
-
-    let users_selection: String =
-        run_with_output(Fzf::default(), out_dirs).expect("Something went awry!");
+fn fzf_search(out_dirs: HashSet<String>) -> String {
+    let users_selection: String = run_with_output(
+        Fzf::builder()
+            .border(Border::Rounded)
+            .border_label("Sessionizer")
+            .color(Color::Dark)
+            .build()
+            .unwrap(),
+        out_dirs,
+    )
+    .expect("Something went awry with fzf!");
     if users_selection.is_empty() {
         std::process::exit(0)
     }
-    tmux_session(users_selection);
+    users_selection
 }
